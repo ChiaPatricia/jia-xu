2ddf7e527100db1c22aa1c970413f52da234ff29
unknown <chiapatrisha@gmail.com>
2023-08-18 16:15:25 -0700

    updated static

--- themes/hugo-apero/static/js/particles.js
+++ themes/hugo-apero/static/js/particles.js
@@ -1,72 +1,70 @@
-particlesJS("particles-js", {
+particlesJS('particles-js', {
   particles: {
-    number: { value: 100, density: { enable: true, value_area: 800 } },
-    color: { value: "#d33682" },
+    number: {
+      value: 200,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    color: {
+      value: '#ffffff'
+    },
     shape: {
-      type: "polygon",
-      stroke: { width: 0, color: "#000000" },
-      polygon: { nb_sides: 6 },
-      image: { src: "img/github.svg", width: 100, height: 100 }
+      type: 'circle',
+      stroke: {
+        width: 0,
+        color: '#000000'
+      }
     },
     opacity: {
-      value: 0.3,
-      random: true,
-      anim: { enable: false, speed: 0.8, opacity_min: 0.1, sync: false }
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: true,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false
+      }
     },
     size: {
-      value: 15,
+      value: 3,
       random: true,
-      anim: { enable: false, speed: 20, size_min: 0.1, sync: false }
-    },
-    line_linked: {
-      enable: true,
-      distance: 100,
-      color: "#2aa198",
-      opacity: 0.3,
-      width: 4
+      anim: {
+        enable: true,
+        speed: 10,
+        size_min: 0.1,
+        sync: false
+      }
     },
     move: {
       enable: true,
-      speed: 2.5,
-      direction: "none",
+      speed: 2,
+      direction: 'top',
       random: false,
       straight: false,
-      out_mode: "out",
-      bounce: false,
-      attract: { enable: false, rotateX: 600, rotateY: 1200 }
+      out_mode: 'out',
+      bounce: false
     }
   },
   interactivity: {
-    detect_on: "canvas",
+    detect_on: 'canvas',
     events: {
-      onhover: { enable: true, mode: "repulse" },
-      onclick: { enable: false, mode: "push" },
+      onhover: {
+        enable: true,
+        mode: 'bubble'
+      },
       resize: true
     },
     modes: {
-      grab: { distance: 400, line_linked: { opacity: 1 } },
-      bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-      repulse: { distance: 200, duration: 0.4 },
-      push: { particles_nb: 4 },
-      remove: { particles_nb: 2 }
+      bubble: {
+        distance: 40,
+        size: 4,
+        duration: 2,
+        opacity: 0.8,
+        speed: 3
+      }
     }
   },
   retina_detect: true
 });
-var count_particles, stats, update;
-stats = new Stats();
-stats.setMode(0);
-stats.domElement.style.position = "absolute";
-stats.domElement.style.left = "0px";
-stats.domElement.style.top = "0px";
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector(".js-count-particles");
-update = function () {
-  stats.begin();
-  stats.end();
-  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-  }
-  requestAnimationFrame(update);
-};
-requestAnimationFrame(update);
